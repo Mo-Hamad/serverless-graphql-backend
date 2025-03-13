87208a8d185b77e9aebc2ea0eb275950bc5ce463
Mo-Hamad <mohamedwabuzaid@gmail.com>
2025-03-12 23:16:19 -0500

    basic structure for apollo server with updated dependencies

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+    }`;
+const resolvers = {
+    Query: {
+        hello: () => 'world',
+    },
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
